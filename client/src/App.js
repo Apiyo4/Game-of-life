86ae542ffb316a95fccab84d49b621f1a76f97fe
Annette Odingo <odingoannette@gmail.com>
2020-06-25 11:07:01 +0100

    Add selectCell functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import Grid from './Grid';
 
+const arrayClone= (arr)=>{
+  return JSON.parse(JSON.stringify(arr))
+}
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.speed = 50;
     this.rows = 25;
     this.columns = 25;
     this.state = {
       generation: 0,
-      gridArray : Array(this.rows).fill().map(()=>Array(this.columns).fill(false))
-    }
+      gridArray: Array(this.rows)
+        .fill()
+        .map(() => Array(this.columns).fill(false)),
+    };
   }
-  
-  render(){
-  
+  selectCell = (row, column) => {
+    let gridArrayClone = arrayClone(this.state.gridArray);
+    gridArrayClone[row][column] = !gridArrayClone[row][column]
+    this.setState({
+      gridArray : gridArrayClone
+    })
+  };
+  render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -24,7 +34,12 @@ class App extends Component {
         <div className="App-flex">
           <div className="App-generation">
             <h2>Generation : {this.state.generation}</h2>
-            <Grid gridArray={this.state.gridArray} rows={this.rows} columns={this.columns} selectCell = {this.selectCell}/>
+            <Grid
+              gridArray={this.state.gridArray}
+              rows={this.rows}
+              columns={this.columns}
+              selectCell={this.selectCell}
+            />
           </div>
           <div className="App-rules">
             <h2>Rules</h2>
@@ -56,15 +71,14 @@ class App extends Component {
             how a few simple rules can result in incredibly complex behaviour.
           </p>
           <p>
-            It contains a square grid that holds cells that are either alive or dead. The
-            behaviour of each cell is dependent only on the state of its eight
-            immediate neighbours, according to the above rules.
+            It contains a square grid that holds cells that are either alive or
+            dead. The behaviour of each cell is dependent only on the state of
+            its eight immediate neighbours, according to the above rules.
           </p>
         </div>
       </div>
     );
   }
-  
 }
 
 export default App;
