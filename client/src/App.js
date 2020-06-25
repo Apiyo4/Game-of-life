97e44ffe21eea1fb65816024591820895d4c56d6
Annette Odingo <odingoannette@gmail.com>
2020-06-25 03:30:59 +0100

    Finish main page layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Grid from './Grid';
 
 class App extends Component {
   constructor(){
@@ -12,11 +13,48 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h2>John Cornway's Game of Life</h2>
+          <h2>Conway's Game of Life</h2>
         </header>
-        <footer className="App-footer">
-    <h4>Generation : {this.state.generation}</h4>
-        </footer>
+        <div className="App-flex">
+          <div className="App-generation">
+            <h4>Generation : {this.state.generation}</h4>
+            <Grid />
+          </div>
+          <div className="App-rules">
+            <h4>Rules</h4>
+            <ul>
+              <li>
+                Any live cell with fewer than two live neighbours dies, as if by
+                underpopulation.
+              </li>
+              <li>
+                Any live cell with two or three live neighbours lives on to the
+                next generation.
+              </li>
+              <li>
+                Any live cell with more than three live neighbours dies, as if
+                by overpopulation.
+              </li>
+              <li>
+                Any dead cell with exactly three live neighbours becomes a live
+                cell, as if by reproduction.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="App-about">
+          <h2>About</h2>
+          <p>
+            The Game of Life is a two dimensional universe in which patterns
+            evolve through time. It is one of the best examples in science of
+            how a few simple rules can result in incredibly complex behaviour.
+          </p>
+          <p>
+            It contains a square grid that holds cells that are either alive or dead. The
+            behaviour of each cell is dependent only on the state of its eight
+            immediate neighbours, according to the above rules.
+          </p>
+        </div>
       </div>
     );
   }
---
+++ client/src/Grid.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Grid extends Component{
+    render(){
+        return(
+            <div>Grid</div>
+        )
+    }
+}
+export default Grid;
