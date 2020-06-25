7eee4714b6ec0adbd97192c2000ee8e9aabd25ac
Annette Odingo <odingoannette@gmail.com>
2020-06-25 02:42:23 +0100

    Add generation to state and display it as a h4 element

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      generation: 0,
+    }
+  }
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <h2>John Cornway's Game of Life</h2>
         </header>
+        <footer className="App-footer">
+    <h4>Generation : {this.state.generation}</h4>
+        </footer>
       </div>
     );
   }
