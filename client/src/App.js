d0e2da982a98c030bb113a4fbd05c9de0cd5b148
Annette Odingo <odingoannette@gmail.com>
2020-06-25 02:37:40 +0100

    Refactor code to use class syntax

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>John Cornway's Game of Life</h2>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>John Cornway's Game of Life</h2>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
