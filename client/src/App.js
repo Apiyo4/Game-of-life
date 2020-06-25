f19d49a3728baaad20180049324807404a360804
Annette Odingo <odingoannette@gmail.com>
2020-06-25 02:31:31 +0100

    Create a h2 element with the games title

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>John Cornway's Game of Life</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
