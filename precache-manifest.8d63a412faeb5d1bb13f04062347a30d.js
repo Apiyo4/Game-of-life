5f674a97de93647612c71abf2ce0b3ee6a4c6810
Annette Odingo <odingoannette@gmail.com>
2020-06-26 08:16:27 +0100

    Updates

---
+++ precache-manifest.8d63a412faeb5d1bb13f04062347a30d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ee372c69bc29fea9da3743eca254fd",
+    "url": "/Game-of-life/index.html"
+  },
+  {
+    "revision": "f05d2ded3df48bce167e",
+    "url": "/Game-of-life/static/css/main.7913d868.chunk.css"
+  },
+  {
+    "revision": "52c63f523afd75e2b78d",
+    "url": "/Game-of-life/static/js/2.07b6aa7b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Game-of-life/static/js/2.07b6aa7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05d2ded3df48bce167e",
+    "url": "/Game-of-life/static/js/main.c5f9e9ae.chunk.js"
+  },
+  {
+    "revision": "e771fb94303cc7ee5f15",
+    "url": "/Game-of-life/static/js/runtime-main.7b022174.js"
+  },
+  {
+    "revision": "4fda14e0d7b4196e757074518b178a57",
+    "url": "/Game-of-life/static/media/img1.4fda14e0.jpg"
+  }
+]);
